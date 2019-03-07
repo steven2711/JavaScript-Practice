48b6c39c5fa791299335198076015ac78a34a99d
Steven Ray Watson <swatson.emu@gmail.com>
2019-03-07 17:46:35 -0600

    Creating web servers and making http requests.

--- node_practice.js
+++ node_practice.js
@@ -109,3 +109,136 @@ fs.readFile("./Desktop/poop.txt", "UTF-8", (err, contents) => {
 })
 
 */
+
+
+
+
+
+
+/////////////////// HTTP Module ////////////////////////
+
+// Making a request
+
+/*
+let https = require("https");
+let fs = require("fs");
+
+
+let options = {
+    hostname: "en.wikipedia.org",
+    port: 443,
+    path: "/wiki/George_Washington",
+    method: "GET"
+};
+
+
+let req = https.request(options, (res) => {
+    let responseBody = "";
+
+    console.log("Response has started.");
+    console.log(`Server Satus: ${res.statusCode}`);
+    console.log("Response Headers: %j", res.headers);
+
+    res.setEncoding("UTF-8");
+
+    res.once("data", (chunk) => {
+
+        console.log(chunk);
+
+    });
+
+    res.on("data", (chunk) => {
+        console.log(`--chunk-- ${chunk.length}`);
+        responseBody += chunk;
+    });
+
+    res.on("end", () => {
+        fs.writeFile("george-washington.html", responseBody, (err) => {
+            if (err) {
+                throw err;
+            };
+
+            console.log("File Downloaded");
+
+        });
+    });
+
+});
+
+req.on("error", () => {
+    console.log(`Problem with request: ${err.message}.`)
+});
+
+req.end();
+
+
+*/
+
+////////////////////////// Building a Web Server ////////////////////////////
+/*
+
+let http = require("http");
+
+let server = http.createServer((req, res) => {
+
+    res.writeHead(200, {"Content-Type": "text/plain"});
+
+    res.end("Hello world.");
+});
+
+server.listen(3000);
+
+
+console.log("Server listening on port 3000");
+
+
+*/
+
+
+
+////////////////////// Serving JSON Data ///////////////
+
+let http = require("http");
+let data = require("./inventory");
+
+http.createServer((req, res) => {
+
+    if (req.url === "/") {
+        res.writeHead(200, {"Content-Type": "text/json"});
+        res.end(JSON.stringify(data));
+    } else if (req.url === "/instock") {
+        listInStock(res);
+    } else if (req.url === "/onorder") {
+        listOnBackOrder(res);
+    } else {
+        res.writeHead(404, {"Content-Type": "text/plain"});
+        res.end("Whoops.....data not found");
+    };
+
+}).listen(3000);
+
+console.log("Server listening on port 3000");
+
+// Functions that filter the JSON file for specified data.
+
+let listInStock = (res) => {
+    let inStock = data.filter((item) => {
+        return item.avail === "In stock";
+    });
+
+    res.end(JSON.stringify(inStock));
+};
+
+
+let listOnBackOrder = (res) => {
+    let onOrder = data.filter((item) => {
+        return item.avail === "On back order";
+    });
+
+    res.end(JSON.stringify(onOrder));
+
+};
+
+
+// You can run this server through node and display through your browser on 
+// port 3000.
