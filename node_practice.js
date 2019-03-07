48ad92dce00943fa8aa8b598bcca3e86ce452c8d
Steven Ray Watson <swatson.emu@gmail.com>
2019-03-06 18:29:33 -0600

    More node.js practice.

--- node_practice.js
+++ node_practice.js
@@ -2,7 +2,7 @@
 // Contains a path to node and the current working file.
 
 ///////// Working with Time /////////
-
+/*
 let waitTime = 4000;
 let currentTime = 0;
 let waitInterval = 300;
@@ -31,3 +31,81 @@ setTimeout(() => {
 
 process.stdout.write('\n\n\n\n');
 writeWaitingPercent(percentWaited);
+
+
+*/
+
+/////////////// Working with Readline() /////////////////
+/*
+let readline = require('readline');
+let fs = require("fs");
+let rl = readline.createInterface(process.stdin, process.stdout);
+
+
+let realPerson = {
+    name: '',
+    sayings: []
+};
+
+
+
+
+rl.question('What is a real persons name? ', (answer) => {
+   
+    realPerson.name = answer;
+
+    fs.writeFileSync(realPerson.name + ".md", `${realPerson.name}\n===========\n\n\n`); 
+    // Creates a file. Located in Finder.
+
+    rl.setPrompt(`What would ${realPerson.name} say? (type 'exit' to end)`);
+
+    rl.prompt();
+   
+    rl.on('line', (saying) => {
+        realPerson.sayings.push(saying.trim());
+
+        fs.appendFile(realPerson.name + '.md', `* ${saying}\n`);  
+        // Appends to the file each saying.       
+
+        if (saying.toLowerCase().trim() === 'exit') {
+            rl.close();
+        } else {
+            rl.setPrompt(`What else would ${realPerson.name} say? `);
+            rl.prompt();
+        };
+
+   });
+    
+})
+
+*/
+
+
+
+////////////////// Using the exec command ////////////////
+/*
+let exec = require("child_process").exec;
+
+
+exec("open https://facebook.com")   // Opens FB in a new tab
+
+*/
+
+
+
+
+/////////////////// Reading Files //////////////////
+
+/*
+let fs = require("fs");
+
+
+fs.readFile("./Desktop/poop.txt", "UTF-8", (err, contents) => {
+    if (err) {
+        console.log(err);
+    };
+
+    console.log(contents);
+})
+
+*/
